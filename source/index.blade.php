d820b87b79232dcfca6e4f8d66475db77fa79ede
Jay Tyrrell <tyrrell.jay@gmail.com>
2017-10-15 19:55:42 -0400

    Remove two paragraphs

--- source/index.blade.php
+++ source/index.blade.php
@@ -8,8 +8,4 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores itaque nam vitae officiis atque nihil quae dolorum ab pariatur eum, minus. Fugiat corrupti adipisci quibusdam sint totam, necessitatibus minus eaque.</p>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi facilis possimus pariatur itaque temporibus, molestias dolores deserunt assumenda, reiciendis architecto necessitatibus tempore in voluptatibus dignissimos doloremque ad excepturi neque obcaecati. Fugiat corrupti adipisci quibusdam sint totam, necessitatibus minus eaque. Molestias minus soluta animi, amet consequuntur eveniet libero.</p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. At, aliquam. Sit quod esse debitis, dolores molestias fugiat praesentium cumque, alias necessitatibus, reprehenderit suscipit cupiditate similique placeat vel animi aperiam corporis.</p>
-
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus dolor quam amet accusantium omnis accusamus dolorem fugiat dicta, doloremque ullam pariatur nulla. Molestias minus soluta animi, amet consequuntur eveniet libero.</p>
 @endsection
