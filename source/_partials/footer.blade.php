93da20996d7193fad1e6de42dfce1812f34a4095
Jay Tyrrell <tyrrell.jay@gmail.com>
2018-03-22 21:00:24 -0400

    Add text to homepage

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,5 +1,13 @@
-<footer class="footer mt3">
+<footer class="footer mt4">
     <div class="container">
-        Copyright &copy; Jay Tyrrell, {{ date('Y') }}
+        <div class="social">
+            <a href="mailto:jay@jaytyrrell.co">Email</a> -
+            <a href="https://github.com/jaytyrrell13" target="_blank">GitHub</a> -
+            <a href="https://www.linkedin.com/in/jaytyrrell" target="_blank">LinkedIn</a>
+        </div>
+
+        <div class="copyright">
+            Copyright &copy; Jay Tyrrell, {{ date('Y') }}
+        </div>
     </div>
 </footer>
--- source/index.blade.php
+++ source/index.blade.php
@@ -5,7 +5,15 @@
         <h1>Hey, I'm Jay Tyrrell,<br>Senior Software Engineer at <a href="http://ableto.com" target="_blank">AbleTo</a></h1>
     </div>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores itaque nam vitae officiis atque nihil quae dolorum ab pariatur eum, minus. Fugiat corrupti adipisci quibusdam sint totam, necessitatibus minus eaque.</p>
+    <div class="headshot">
+        <img src="/images/headshot.jpg" alt="Picture of me" class="bordered circle" />
+    </div>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi facilis possimus pariatur itaque temporibus, molestias dolores deserunt assumenda, reiciendis architecto necessitatibus tempore in voluptatibus dignissimos doloremque ad excepturi neque obcaecati. Fugiat corrupti adipisci quibusdam sint totam, necessitatibus minus eaque. Molestias minus soluta animi, amet consequuntur eveniet libero.</p>
+    <p>I'm originally from Pompano Beach, Florida but have lived in the New York area since May, 2012. I am currently living in Brooklyn but previously lived in Jersey City for almost two years.</p>
+
+    <p>In October, 2010 I started working at EDB Solutions. My responsibilities included designing and coding client sites for small businesses located nationwide. When I moved to New York, I continued working remotely until the end of July.</p>
+
+    <p>In December of that year, I started part-time as an intern at CourseHorse. My responsibilities included creating newsletters and testing the site for quality assurance. After about a month, I also started working part-time as a QA Engineer at MEV. In August, 2013 I decided to join full-time as Software Engineer at CourseHorse. My responsibilities included building/deploying new features, developing tools to make development more efficient, and fixing bugs.</p>
+
+    <p>In February, 2016 I started working at AbleTo.</p>
 @endsection
